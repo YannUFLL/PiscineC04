b332d46f56388452a10f30df9033b6dfa0f82d55
Yann Dumaine <yannlescoul79f>
2022-01-29 13:35:32 +0100

    initialsisation de i

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,35 +6,11 @@
 /*   By: ydumaine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 11:38:12 by ydumaine          #+#    #+#             */
-/*   Updated: 2022/01/27 17:51:06 by ydumaine         ###   ########.fr       */
+/*   Updated: 2022/01/29 13:33:19 by ydumaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-int	ft_power(int a, int j)
-{
-	j--;
-	if (j <= 0)
-		return (1);
-	else
-		a = ft_power(a, j) * 10;
-	return (a);
-}
-
-int	ft_converter(char *str, int a, int i)
-{
-	int	b;
-
-	b = 0;
-	while (str[i] >= 48 && str[i] <= 57)
-	{
-		b = b + (str[i] - 48) * -a;
-		a = a / 10;
-		i++;
-	}
-	return (b);
-}
 
 int	ft_pair_or_impair(char *str, int i)
 {
@@ -57,14 +33,13 @@ int	ft_pair_or_impair(char *str, int i)
 int	ft_atoi(char *str)
 {
 	int	i;
-	int	j;
 	int	a;
 	int	c;
 	int	b;
 
-	j = 0;
-	i = 0;
+	b = 0;
 	a = 0;
+	i = 0;
 	while ((str[i] > 8 && str[i] < 14) || str[i] == 32)
 			i++;
 	c = ft_pair_or_impair(str, i);
@@ -74,11 +49,10 @@ int	ft_atoi(char *str)
 		a = i;
 	while (str [i] >= 48 && str[i] <= 57)
 	{
+		b = b * 10 - (str[i] - 48);
 		i++;
-		j++;
 	}
-	b = ft_converter(str, ft_power(-1, j), a);
-	if (c == 1 && b < 0)
-		b = -b ;
+	if (c == 1)
+		b = -b;
 	return (b);
 }
